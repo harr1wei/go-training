dd722ffbfbb39f0c266268f27ba2a554bb628715
weiyongjie <weiyongjie8253@dingtalk.com>
2023-11-08 11:26:34 +0800

    “update”

--- guess_num/main.go
+++ guess_num/main.go
@@ -3,12 +3,22 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
+	"os/signal"
 	"strconv"
+	"syscall"
 	"time"
 )
 
+var interrupt = make(chan os.Signal)
+
 func main() {
-	foo()
+	signal.Notify(interrupt, syscall.SIGINT, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGKILL)
+	go foo()
+	select {
+	case <-interrupt:
+		return
+	}
 }
 
 func foo() {
@@ -16,18 +26,17 @@ func foo() {
 		rand.Seed(time.Now().UnixNano())
 		randNum := rand.Intn(100) + 1
 
-		var input string
 		fmt.Println("Hello, please input a guess number")
 	loop:
+		input := ""
 		fmt.Scanln(&input)
-
 		if input == "stop" {
 			return
 		}
 
 		i, err := strconv.Atoi(input)
 		if err != nil {
-			fmt.Println("input error: ", err)
+			goto loop
 		}
 
 		switch {
@@ -38,7 +47,7 @@ func foo() {
 			fmt.Println("greater than the number, try again")
 			goto loop
 		case i == randNum:
-			fmt.Println("great! you did the right choice")
+			fmt.Printf("great! you did the right choice, number is %d\n\n", i)
 		}
 	}
 }
